9833f1f3aaf1ee7c7cac11b8bcc4dfb51f54fffb
Suvendu <suvu0707@gmail.com>
2023-08-07 20:50:10 +0530

    sff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'reactofy-css-library/styles/buttonIcon.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      
+        <p className='container title'>
+          FFFFFF
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
     </div>
   );
 }
